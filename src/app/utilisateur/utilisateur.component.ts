8276635940b55d9e8aa6b2159eeb19456ee31391
thabet-fix <thabet_jmal@yahoo.fr>
2018-11-20 23:04:57 +0100

    component Compte testing

--- src/app/utilisateur/utilisateur.component.ts
+++ src/app/utilisateur/utilisateur.component.ts
@@ -18,24 +18,49 @@ export class UtilisateurComponent implements OnInit {
   prenom: string;
   utilisateurs: Utilisateur[];
   utilisateur: Utilisateur;
+  originUtilisateur: Utilisateur;
   inputResume: string;
 
   ngOnInit() {
     this.utilisateurService.utilisateurChanged.subscribe(datas => {
       console.log(datas)
       this.utilisateurs = datas;
-      this.utilisateur = this.utilisateurs[0];
-      this.typeContrat = this.utilisateur.type_contrat;
-      this.utilisateurService.setDocUtilisateurId(this.utilisateur.$key);
+      this.originUtilisateur = this.utilisateurs[0];
+      this.typeContrat = this.originUtilisateur.type_contrat;
+      this.utilisateurService.setDocUtilisateurId(this.originUtilisateur.$key);
       console.log("utl init >> ")
-      console.log(this.utilisateur)
-
+      console.log(this.originUtilisateur)
+      this.utilisateur = this.originUtilisateur;
+      //this.initUtilisateur()
     });
     
     this.utilisateurService.getUtilisateur();
+    //this.utilisateur = this.originUtilisateur;
   }
 
+  initUtilisateur(){
+
+    console.log("Object >> ")
+    console.log(this.formProfil)
+    console.log("Motivation 0 >> ")
+    console.log(this.formProfil.value.axeMotivation)
+    //this.utilisateur['resume'] = this.formProfil.value.resume;
+    console.log("Motivation 1 >> ")
+    console.log(this.formProfil.value.resume)
+    //this.utilisateur['axe_motivation'] = this.formProfil.value.axeMotivation;
+    console.log("Motivation 2 >> ")
+    console.log(this.formProfil.value.resume3)
+  }
+  changeValeur( desc, value ) {
+    for (var i in this.utilisateur) {
+      if (this.utilisateur[i].value == value) {
+        this.utilisateur[i].desc = desc;
+         break; //Stop this loop, we found it!
+      }
+    }
+ }
   onClickEnregistrerResume(form: NgForm){
+    this.initUtilisateur()
     this.utilisateur.resume = form.value.resume;
     console.log("Motivation >> "+this.utilisateur.axe_motivation);
     this.utilisateurService.modifierUtilisateur(this.utilisateur);
